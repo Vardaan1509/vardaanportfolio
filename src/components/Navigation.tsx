1bc8d232f49e03847c506ec349eb1a88d79eecf2
Vardaan Mehandiratta <vardaanmehandiratta926@gmail.com>
2025-11-13 23:39:02 -0500

    Update Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -78,8 +78,7 @@ const Navigation = () => {
                 size="sm"
                 className="bg-primary hover:bg-primary-light text-primary-foreground w-fit shadow-elegant hover-lift"
               >
-                <Mail className="w-4 h-4 mr-2" />
-                Get In Touch
+                
               </Button>
             </div>
           </div>
